ab740ae511faa949f9a8ae89b06b698d888c2bd8
vvictorious <vvictorious30@gmail.com>
2024-08-01 11:38:30 -0700

    answeered the first queestion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const fruits = [
+    "Apples",
+    "Apricot",
+    "Banana",
+    "Blueberry",
+    "Cherry",
+    "Cranberry",
+    "Date",
+    "Dragonfruit",
+    "Elderberry",
+    "Fig",
+    "Grape",
+    "Grapefruit",
+  ];
+
+  const [inputValue, setInputValue] = useState("");
+
+  const filteredFruits = fruits.filter(fruit => fruit.toLowerCase().includes(inputValue.toLowerCase()))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {filteredFruits.map((fruit, index) => (
+        <p key={index}>{fruit}</p>
+      ))}
     </div>
   );
 }
